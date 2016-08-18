efec41319867992a6dc8d11bb2123354442ac4eb
YOLANDA RODRIGUEZ <Array415@gmail.com>
2016-08-18 16:38:48 -0700

    Users Controller created needs edit and update

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  before_action :logged_in?, only: [:show]
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      login(@user)
+      redirect_to @user
+    else
+      redirect_to ('/')
+      flash[:notice] = "email already exists"
+    end
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    if params[:id].to_s == current_user[:id].to_s
+      render :show
+    else
+      redirect_to ('/')
+      flash[:notice] = "You are not Authorized for this profile"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first, :last, :email, :password, :username, :curr_city)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'welcome#splash'
 
   get '/' => 'welcome#splash'
+
+  get '/signup', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#show', as: 'user'
 end
