fb77503db62b0a9d9e7f1a85732be8f5f0796a17
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air-1.local>
2016-08-18 17:01:35 -0700

    generated controller and added new create and destroy.Made view#new. Also added corresponding routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: 'welcome#splash'
 
   get '/' => 'welcome#splash'
+
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create"
+
 end
