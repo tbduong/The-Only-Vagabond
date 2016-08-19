9b4a89bf43126c2cf430d988d3d47e76743bc7e4
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air-1.local>
2016-08-19 15:57:26 -0700

    added controller methods and associated routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   post '/sessions', to: 'sessions#create'
 
   get '/comments', to: 'comments#index', as: 'comments'
+  get "/comments/new", to: "comments#new", as: "new_comment"
+  post "/comments", to: "comments#create"
+  delete "/comments/:id", to: "comments#destroy"
+  get "/comments/:id", to: "comment#show", as: "comment"
 end
