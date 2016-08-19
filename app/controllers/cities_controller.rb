350ebb09c1bf8a85debfdd782a63d48d508e078a
tbduong <tiffaniduong@gmail.com>
2016-08-19 16:10:01 -0700

    cities controller and routes up and running, index and show renders

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,25 @@
 class CitiesController < ApplicationController
+  def index
+    @cities = City.all
+    render :index
+  end
+
+  def new
+    @city = City.new
+    render :new
+  end
+
+  def create
+    @city = City.create(city_params)
+  end
+
+  def show
+    @city = City.find(params[:id])
+    render :show
+  end
+
+  def destroy
+    @city.delete(params[:id])
+  end
+
 end
